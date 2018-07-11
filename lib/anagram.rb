31ae6bdccd9d22a66669a949c39c95729d9aec93
Wendy Calderon <wendycalderon@gmail.com>
2018-07-11 22:42:18 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -15,5 +15,10 @@ class Anagram
       result << i if i.split(//).sort == sorted_word
     end
     result
+=======
+    sorted_word = @word
+sorted_word.split(//)
+    binding.pry
+>>>>>>> 5c7f7f2aef7a4c2846cf44361164200bc79340d0
   end
 end
